0901ba238b31e6b91380950fa48ede54bee25bdd
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-18 18:25:59 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-def greeting 
-  return "Hello World"
-end
+greeting = "Hello World"
 
 puts greeting 
